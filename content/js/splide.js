b322a55d9c718351aab011a0deedc29e76150cd8
ali <ali@example.com>
2024-07-28 14:05:00 +0300

    update

---
+++ content/js/splide.js
@@ -0,0 +1,20 @@
+function splideTeam() {
+    // Target the component using its classes
+    let splides = document.querySelectorAll('.splide.is-team');
+    for (let i = 0, splideLength = splides.length; i < splideLength; i++) {
+        // Initialize the slider with custom options
+        new Splide(splides[i], {
+            type: 'loop',
+            gap: '.5rem',
+            autoWidth: true,
+            arrows: false,
+            pagination: false,
+            drag: false,
+            autoScroll: {
+                speed: 1,
+                pauseOnHover: false // Disable pausing on hover
+            }
+        }).mount(window.splide.Extensions);
+    }
+}
+splideTeam();
